1929f031cf95942df7863e5c1b89f68b43fecfa4
Aurelia Amanda Robert <aureliaamanda12@gmail.com>
2025-07-27 02:55:31 +0700

    fix testing

--- src/test/java/com/example/ebankingportal/EbankingPortalApplicationTests.java
+++ src/test/java/com/example/ebankingportal/EbankingPortalApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.ebankingportal;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@@SpringBootTest
+@SpringBootTest
 class EbankingPortalApplicationTests extends AbstractIntegrationTest {
 
 	@Test
