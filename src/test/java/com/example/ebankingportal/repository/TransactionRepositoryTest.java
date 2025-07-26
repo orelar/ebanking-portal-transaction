5e4830613eabd458fb61c01cf5b06788b92f3c4d
Aurelia Amanda Robert <aureliaamanda12@gmail.com>
2025-07-27 03:25:33 +0700

    testing circleci

--- src/test/java/com/example/ebankingportal/repository/TransactionRepositoryTest.java
+++ src/test/java/com/example/ebankingportal/repository/TransactionRepositoryTest.java
@@ -1,10 +1,11 @@
 package com.example.ebankingportal.repository;
 
+import com.example.ebankingportal.AbstractIntegrationTest;
 import com.example.ebankingportal.model.Transaction;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
@@ -14,30 +15,31 @@ import java.time.Month;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@DataJpaTest
-class TransactionRepositoryTest {
-
-    @Autowired
-    private TestEntityManager entityManager;
+@SpringBootTest
+class TransactionRepositoryTest extends AbstractIntegrationTest {
 
     @Autowired
     private TransactionRepository transactionRepository;
 
+    @AfterEach
+    void tearDown() {
+        transactionRepository.deleteAll();
+    }
+
     @Test
     void findByCustomerIdAndValueDateBetween_ReturnsCorrectTransactions() {
-        String customerId = "CUST-123";
-        entityManager.persist(new Transaction("t1", customerId, "IBAN-1", BigDecimal.TEN, "USD", LocalDate.of(2025, Month.JULY, 15), "July Transaction"));
-        entityManager.persist(new Transaction("t2", customerId, "IBAN-1", BigDecimal.TEN, "USD", LocalDate.of(2025, Month.JUNE, 15), "June Transaction"));
+        String customerId = "CUSTOMER-123";
+
+        transactionRepository.save(new Transaction("t7", customerId, "IBAN-1", BigDecimal.TEN, "USD", LocalDate.of(2025, Month.JULY, 15), "July Transaction"));
+        transactionRepository.save(new Transaction("t8", customerId, "IBAN-1", BigDecimal.TEN, "USD", LocalDate.of(2025, Month.JUNE, 15), "June Transaction"));
 
         LocalDate startDate = LocalDate.of(2025, Month.JULY, 1);
         LocalDate endDate = LocalDate.of(2025, Month.JULY, 31);
         PageRequest pageable = PageRequest.of(0, 10);
 
-        // Act
         Page<Transaction> result = transactionRepository.findByCustomerIdAndValueDateBetween(customerId, startDate, endDate, pageable);
 
-        // Assert
         assertEquals(1, result.getTotalElements());
-        assertEquals("t1", result.getContent().get(0).getId());
+        assertEquals("t7", result.getContent().get(0).getId());
     }
 }
